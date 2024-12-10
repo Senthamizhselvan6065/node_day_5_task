643da73276cd176670709c729d583afe6c6cec9f
Senthamizhselvan6065 <senthamizhselvan6065@gmail.com>
2024-12-11 03:04:12 +0530

    feat : login component created

--- frontend/src/components/SignIn.jsx
+++ frontend/src/components/SignIn.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { MdOutlineAlternateEmail } from "react-icons/md";
+import { TbPasswordUser } from "react-icons/tb";
+import style from './card.module.css';
 
 const SignIn = () => {
   return (
-    <div>SignIn</div>
+    <div className={style.container}>
+        <div className={style.card}>
+            <div className={style.left_card}>
+                <h1 className={style.left_card_title}>Welcome Back...</h1>
+                <p className={style.left_card_desc}>Welcome back! please enter your details.</p>
+            </div>
+            <div className={style.right_card}>
+                <h1 className={style.right_card_title}>Sign In</h1>
+                <form className={style.form}>
+                    <div className={style.form_input_group}>
+                        <span className={style.form_input_group_icon}><MdOutlineAlternateEmail /></span>
+                        <input className={style.form_input_group_input} type="text" placeholder='Email...'/>
+                    </div>
+                    <div className={style.form_input_group}>
+                        <span className={style.form_input_group_icon}><TbPasswordUser /></span>
+                        <input className={style.form_input_group_input} type="text" placeholder='Password...'/>
+                    </div>
+                    <button className={style.form_btn}>Sign In</button>
+                </form>
+                <p className={style.desc}><span className={style.nav_link}>Forgot Password!</span></p>
+                <p className={style.desc}>Don't have an account?<span className={style.nav_link}>Sign up</span></p>
+            </div>
+        </div>
+    </div>
   )
 }
 
