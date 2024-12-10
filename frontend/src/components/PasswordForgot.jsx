47f835e8a257348677866ab01dccfaeb9bc23dee
Senthamizhselvan6065 <senthamizhselvan6065@gmail.com>
2024-12-11 03:54:52 +0530

    fead : added forgot and reset password components created

--- frontend/src/components/PasswordForgot.jsx
+++ frontend/src/components/PasswordForgot.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
-
+import React from 'react';
+import style from './card.module.css';
+import { MdOutlineAlternateEmail } from "react-icons/md";
+import passwordImg from '../assets/password.png';
 const PasswordForgot = () => {
   return (
-    <div>PasswordForgot</div>
+    <div className={style.container}>
+        <div className={style.card}>
+            <div className={style.left_card}>
+                <img className={style.left_card_img} src={passwordImg} />
+            </div>
+            <div className={style.right_card}>
+                  <h1 className={style.right_card_title}>Forgot Password</h1>
+                  <p className={style.right_card_desc}>Enter the email address associted with your account and we'll send you a link to reset your password.</p>
+                <form className={style.form}>
+                    <div className={style.form_input_group}>
+                        <span className={style.form_input_group_icon}><MdOutlineAlternateEmail /></span>
+                        <input className={style.form_input_group_input} type="text" placeholder='Email...'/>
+                    </div>
+                    <button className={style.form_btn}>Send</button>
+                </form>
+                <p className={style.desc}><span className={style.nav_link}>Back to Login</span></p>
+            </div>
+        </div>
+    </div>
   )
 }
 
