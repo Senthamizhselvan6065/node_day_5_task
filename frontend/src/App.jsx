3516e4589fc02e5fe031af91ca82bcc816f7a8ed
Senthamizhselvan6065 <senthamizhselvan6065@gmail.com>
2024-12-11 04:25:32 +0530

    feat : configure react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React from 'react';
+import Hero from './components/Hero';
+import SignUp from './components/SignUp';
+import SignIn from './components/SignIn';
+import PasswordForgot from './components/PasswordForgot';
+import PasswordReset from './components/PasswordReset';
+import PageNotFound from './components/PageNotFound';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const App = () => {
+    
+    const router = createBrowserRouter([
+       {
+         path: "/",
+         element: <Hero />
+       },
+       {
+         path: "/sign_up",
+         element: <SignUp />
+       },
+       {
+         path: "/sign_in",
+         element: <SignIn />
+       },
+       {
+         path: "/password/forgot",
+         element: <PasswordForgot />
+       },
+       {
+         path: "/password/reset/:token",
+         element: <PasswordReset />
+       },
+       {
+         path: "*",
+         element: <PageNotFound />
+       },
+    ]);
+
   return (
-    <div>App</div>
+    <div>
+        <RouterProvider router={router} />
+    </div>
   )
 }
 
