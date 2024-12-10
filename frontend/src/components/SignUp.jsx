db2c9092352d53b233de8d0dc5576fd56a2d5c1e
Senthamizhselvan6065 <senthamizhselvan6065@gmail.com>
2024-12-11 02:39:33 +0530

    feat : register form added

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import style from './card.module.css';
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div className={style.container}>
+        <div className={style.card}>
+            <div className={style.left_card}>
+                <h1 className={style.left_card_title}>Hello, Friends!</h1>
+                <p className={style.left_card_desc}>Register with your personal details to use all of site features</p>
+            </div>
+            <div className={style.right_card}>
+                <h1 className={style.right_card_title}>Sign Up</h1>
+                <form className={style.form}>
+                     <input className={style.input} type="text" placeholder='Username...'/>
+                     <input className={style.input} type="text" placeholder='Email...'/>
+                     <input className={style.input} type="text" placeholder='Password...'/>
+                     <button className={style.form_btn}>Sign up</button>
+                </form>
+                <p className={style.desc}>Already have an acccount? <span className={style.nav_link}>Sign In</span></p>
+            </div>
+        </div>
+    </div>
   )
 }
 
