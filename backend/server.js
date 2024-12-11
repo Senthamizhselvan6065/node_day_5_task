54778093c48844aae26a33464dd96b6b8548c14d
Senthamizhselvan6065 <senthamizhselvan6065@gmail.com>
2024-12-11 08:51:28 +0530

    feat : database configuration

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ const NODE_SERVER = express();
 const dotenv = require('dotenv');
 dotenv.config();
 
+/* coneect mongodb */
+const connectMongoDb = require('./config/database');
+connectMongoDb();
+
 /* appp server */
 const APP_SERVER = require('./app');
 NODE_SERVER.use('/', APP_SERVER);
